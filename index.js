5a4c35ecb57bb0233a384f25924917193c0423b0
jb1113 <leftydev1113@gmail.com>
2021-02-17 20:54:58 +0900

    javascript let, const, var

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("I'm Working. I'm JS. I'm Beautiful. I'm worth it");
 
-let a = 100;
+const a = 100;
 let b = a + 50;
-a = 200;
+// a = 200; Assignment to constant variable
 console.log(b, a);
