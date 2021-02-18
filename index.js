9b4a1e3ec2b58a793e994bbe617e940fb3fb2b37
jb1113 <leftydev1113@gmail.com>
2021-02-18 20:16:25 +0900

    javascript data type

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-console.log("I'm Working. I'm JS. I'm Beautiful. I'm worth it");
+// String, "text"
+const name = "JB";
+console.log(name)
 
-const a = 100;
-let b = a + 50;
-// a = 200; Assignment to constant variable
-console.log(b, a);
+// Boolean, true or false
+const married = false;
+console.log(married);
+
+// Number, 12345
+const age = 30;
+console.log(age);
+
+// Float 3.141592
+const PI = 3.14;
+console.log(PI);
