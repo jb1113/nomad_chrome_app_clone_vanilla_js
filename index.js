9badcf8cb2c22629c798336dc267d1cec7bbfe6f
jb1113 <leftydev1113@gmail.com>
2021-02-20 13:51:13 +0900

    fix practice code

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+console.log("I'm Working. I'm JS. I'm Beautiful. I'm worth it");
+
+// let a = 100;
+const a = 100;
+
+let b = a + 50;
+
+// a = 200; Assignment to constant variable
+
+console.log(b, a);
+
 // String, "text"
 const name = "JB";
 console.log(name)
